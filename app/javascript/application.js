2259cd1600dcba706abc16c7ea579a6caf4aa6cb
yuler <is.yuler@gmail.com>
2024-11-05 19:19:42 +0800

    Add debug=1 mode for js debug

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,16 @@ import "controllers"
 
 import "trix"
 import "@rails/actiontext"
+
+// is url exist debug=1
+!(function () {
+  if (!/debug=1/.test(window.location)) return
+
+  var src = '//cdn.jsdelivr.net/npm/eruda'
+  var script = document.createElement('script')
+  script.src = src;
+  script.onload = function () {
+    eruda.init();
+  };
+  document.head.appendChild(script);
+})()
