28e541a09f9cb66a94c1d5036549d2c7d8bb785d
yuler <is.yuler@gmail.com>
2025-06-16 17:13:35 +0800

    Update brakeman version and improve code style in pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def about
-    markdown_content = File.read(Rails.root.join('app', 'views', 'pages', 'about.md'))
+    markdown_content = File.read(Rails.root.join("app", "views", "pages", "about.md"))
     renderer = Redcarpet::Render::HTML.new
     markdown = Redcarpet::Markdown.new(renderer, extensions = {})
     @layout_class = "mt-8 prose prose-2xl"
